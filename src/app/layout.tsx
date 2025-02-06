392946b563f53fd60ffc40b64a0ab210d6c5e576
iltan987 <iltancanerr@gmail.com>
2025-02-06 22:29:24 +0200

    initialize project with Next.js, TypeScript, ESLint, Tailwind CSS, and App Router setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+export default RootLayout;
