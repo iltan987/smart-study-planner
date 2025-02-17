4f84b3c1bc89b987627c2374fc75832e74d4628f
İltan CANER <iltancanerr@gmail.com>
2025-02-17 21:48:20 +0300

    Chore/update eslint rules (#11)
    * refactor: update ESLint configuration to use compat.config
    
    * add consistent-type-imports rule to ESLint configuration
    
    * add no-unused-vars rule to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,11 +10,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript',
-    'plugin:prettier/recommended'
-  ),
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
