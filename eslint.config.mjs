b8014eb4fb10602e13f1daed7790bd1ee1f8806f
İltan CANER <iltancanerr@gmail.com>
2025-02-21 13:08:28 +0300

    Feature/setup eslint prettier (#18)
    * install eslint-config-prettier eslint-plugin-prettier prettier packages as dev dependency
    
    * chore: update ESLint configuration to include Prettier integration
    
    * chore: add Prettier configuration files
    
    * chore: add VSCode settings for Prettier formatting on save
    
    * refactor: formatting
    
    * chore: update ESLint configuration to include custom rules for TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
