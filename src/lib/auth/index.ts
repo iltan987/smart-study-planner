4f7f78cd742a71e5d8b2b9a63278240f911d997e
iltan987 <iltancanerr@gmail.com>
2025-02-21 22:47:37 +0200

    feat: integrate Prisma adapter and set JWT as session strategy

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,6 +1,12 @@
 import NextAuth from 'next-auth';
 import config from './config';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '../db';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
   ...config,
 });
