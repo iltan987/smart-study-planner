c49fb9843fdbdb43b89ec3aa4cb016196c7b842e
iltan987 <iltancanerr@gmail.com>
2025-02-24 22:59:42 +0200

    chore: remove unnecessary configurations and update dependencies

--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -1,7 +1,11 @@
 import type { NextAuthConfig } from 'next-auth';
-import { Credentials, Google, GitHub } from './providers';
+import { Google, GitHub } from './providers';
 
 export default {
-  providers: [Credentials, Google, GitHub],
+  providers: [Google, GitHub],
+  trustHost: true,
+  pages: {
+    signIn: '/login',
+  },
   debug: process.env.NODE_ENV === 'development',
 } satisfies NextAuthConfig;
--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -2,11 +2,15 @@ import NextAuth from 'next-auth';
 import config from './config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from '../db';
+import { Credentials } from './providers';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  providers: [Credentials, ...config.providers],
   session: {
     strategy: 'jwt',
   },
-  ...config,
+  trustHost: config.trustHost,
+  pages: config.pages,
+  debug: config.debug,
 });
