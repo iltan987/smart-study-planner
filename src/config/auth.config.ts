d1dc01fae6b883f2d086d761340861ae38f51e4d
iltan987 <iltancanerr@gmail.com>
2025-03-07 20:28:11 +0200

    feat: extend Auth.js session and JWT to include user ID

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -2,8 +2,24 @@ import { InvalidCredentialsError } from '@/errors/InvalidCredentialsError';
 import { loginSchema } from '@/schemas/auth/login.schema';
 import { comparePassword } from '@/utils/crypto.util';
 import { getUserByEmail } from '@/utils/user.util';
-import type { NextAuthConfig } from 'next-auth';
+import type { DefaultSession, NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
+}
+
+import 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string;
+  }
+}
 
 export default {
   providers: [
@@ -34,6 +50,18 @@ export default {
       },
     }),
   ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user?.id) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user.id = token.id;
+      return session;
+    },
+  },
   pages: {
     signIn: '/login',
   },
