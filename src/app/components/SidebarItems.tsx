cb29666998bb4a8ffb12635e4cd9feefde4f7ab3
nazhin1382 <nazhin.abdolbaghi@gmail.com>
2025-03-07 13:02:53 +0200

    logout button to bottom o the page

--- src/app/components/SidebarItems.tsx
+++ src/app/components/SidebarItems.tsx
@@ -6,8 +6,12 @@ interface MenubarItemProps {
 }
 
 export default function MenubarItem({ label, icon, path }: MenubarItemProps) {
+  const isLogout = label === 'LogOut';
   return (
-    <button className="flex items-center p-3 text-lg w-auto text-left cursor-pointer hover:bg-purple-200 mb-2 px-4 rounded-lg">
+    <button
+    className={`flex items-center p-3 text-lg w-auto text-left cursor-pointer 
+      hover:bg-purple-200 mb-2 px-4 rounded-lg 
+      ${isLogout ? 'mt-auto' : ''}`}>
       <span className="text-gray-500 pr-2 flex-shrink-0 ">{icon}</span>
       <span className="text-lg text-black font-normal flex-grow">{label}</span>
     </button>
