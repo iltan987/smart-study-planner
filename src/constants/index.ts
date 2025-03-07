f49b5011d27dcbb4f5d7990e11f516f00e44c98a
iltan987 <iltancanerr@gmail.com>
2025-03-07 14:36:38 +0200

    Delete unneccessary files

--- src/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './response-messages';
-export * from './cookie-constants';
--- src/schemas/auth/session.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const sessionSchema = z.object({
-  id: z.string(),
-});
-
-export type SessionSchema = z.infer<typeof sessionSchema>;
