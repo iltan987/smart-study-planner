65d25648930c68b9d722c6221d976c73338af7fe
iltan987 <iltancanerr@gmail.com>
2025-04-24 23:58:45 +0300

    Refactor Redis utility functions: enhance type safety and update schema handling

--- src/utils/redis.util.ts
+++ src/utils/redis.util.ts
@@ -1,22 +1,39 @@
-import redisClient from '@/lib/redis';
-import { type HistorySchema, historySchema } from '@/schemas/history.schema';
-import { ContentType } from '@prisma/client';
-import 'server-only';
+import { ContentType } from '@/generated/prisma-client';
+import getRedisClient from '@/lib/redis';
+import type {
+  FunctionCallContentGetSchema,
+  FunctionCallContentSchema,
+  FunctionResponseContentGetSchema,
+  FunctionResponseContentSchema,
+  TextContentGetSchema,
+  TextContentSchema,
+} from '@/schemas/history.schema';
+import {
+  functionCallContentGetSchema,
+  functionCallContentSchema,
+  functionResponseContentGetSchema,
+  functionResponseContentSchema,
+  textContentGetSchema,
+  textContentSchema,
+} from '@/schemas/history.schema';
+import { v4 as uuidv4 } from 'uuid';
 
-export async function getTextHistory(userId: string) {
-  const historyEntries: HistorySchema[] = [];
-  const keys = await redisClient.keys(`history:${userId}:text:*`);
+export async function getTextHistory(
+  userId: string
+): Promise<TextContentGetSchema[]> {
+  const historyEntries: TextContentGetSchema[] = [];
+  const redis = await getRedisClient();
+  const keys = await redis.keys(`history:${userId}:text:*`);
 
   for (const key of keys) {
-    const entry = await redisClient.hGetAll(key);
+    const entry = await redis.hGetAll(key);
     historyEntries.push(
-      historySchema.parse({
-        role: entry.role,
+      textContentGetSchema.parse({
+        type: ContentType.text,
+        text: entry.text,
+        timeSent: new Date(entry.timeSent),
         time: new Date(entry.time),
-        content: {
-          type: ContentType.TEXT,
-          textContent: { text: entry.content },
-        },
+        role: entry.role,
       })
     );
   }
@@ -24,23 +41,34 @@ export async function getTextHistory(userId: string) {
   return historyEntries;
 }
 
-export async function getFunctionCallHistory(userId: string) {
-  const historyEntries: HistorySchema[] = [];
-  const keys = await redisClient.keys(`history:${userId}:function_call:*`);
+export async function saveTextHistory(userId: string, data: TextContentSchema) {
+  const redis = await getRedisClient();
+  const parsedData = textContentSchema.parse(data);
+  const key = `history:${userId}:text:${uuidv4()}`;
+
+  await redis.hSet(key, {
+    text: parsedData.text,
+    timeSent: parsedData.timeSent.toISOString(),
+    time: new Date().toISOString(),
+    role: parsedData.role,
+  });
+  await redis.expire(key, 60 * 60 * 24); // Set expiration to 1 day
+}
+
+export async function getFunctionCallHistory(
+  userId: string
+): Promise<FunctionCallContentGetSchema[]> {
+  const historyEntries: FunctionCallContentGetSchema[] = [];
+  const redis = await getRedisClient();
+  const keys = await redis.keys(`history:${userId}:function_call:*`);
 
   for (const key of keys) {
-    const entry = await redisClient.hGetAll(key);
+    const entry = await redis.hGetAll(key);
     historyEntries.push(
-      historySchema.parse({
-        role: entry.role,
+      functionCallContentGetSchema.parse({
         time: new Date(entry.time),
-        content: {
-          type: ContentType.FUNCTION_CALL,
-          functionCallContent: {
-            name: entry.content_name,
-            arguments: JSON.parse(entry.content_arguments),
-          },
-        },
+        name: entry.name,
+        args: JSON.parse(entry.args),
       })
     );
   }
@@ -48,26 +76,57 @@ export async function getFunctionCallHistory(userId: string) {
   return historyEntries;
 }
 
-export async function getFunctionResponseHistory(userId: string) {
-  const historyEntries: HistorySchema[] = [];
-  const keys = await redisClient.keys(`history:${userId}:function_response:*`);
+export async function saveFunctionCallHistory(
+  userId: string,
+  data: FunctionCallContentSchema
+) {
+  const redis = await getRedisClient();
+  const parsedData = functionCallContentSchema.parse(data);
+  const key = `history:${userId}:function_call:${uuidv4()}`;
+  const hSetData: Record<string, string> = {
+    time: new Date().toISOString(),
+    name: parsedData.name,
+    args: JSON.stringify(parsedData.args),
+  };
+
+  await redis.hSet(key, hSetData);
+  await redis.expire(key, 60 * 60 * 24); // Set expiration to 1 day
+}
+
+export async function getFunctionResponseHistory(
+  userId: string
+): Promise<FunctionResponseContentGetSchema[]> {
+  const historyEntries: FunctionResponseContentGetSchema[] = [];
+  const redis = await getRedisClient();
+  const keys = await redis.keys(`history:${userId}:function_response:*`);
 
   for (const key of keys) {
-    const entry = await redisClient.hGetAll(key);
+    const entry = await redis.hGetAll(key);
     historyEntries.push(
-      historySchema.parse({
-        role: entry.role,
+      functionResponseContentGetSchema.parse({
         time: new Date(entry.time),
-        content: {
-          type: ContentType.FUNCTION_RESPONSE,
-          functionResponseContent: {
-            name: entry.content_name,
-            response: JSON.parse(entry.content_response),
-          },
-        },
+        name: entry.name,
+        response: JSON.parse(entry.response),
       })
     );
   }
 
   return historyEntries;
+}
+
+export async function saveFunctionResponseHistory(
+  userId: string,
+  data: FunctionResponseContentSchema
+) {
+  const redis = await getRedisClient();
+  const parsedData = functionResponseContentSchema.parse(data);
+  const key = `history:${userId}:function_response:${uuidv4()}`;
+  const hSetData: Record<string, string> = {
+    time: new Date().toISOString(),
+    name: parsedData.name,
+    response: JSON.stringify(parsedData.response),
+  };
+
+  await redis.hSet(key, hSetData);
+  await redis.expire(key, 60 * 60 * 24); // Set expiration to 1 day
 }
