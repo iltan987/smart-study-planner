3808f29b1575475300b306633c7df4e2e09eb44d
nazhin1382 <nazhin.abdolbaghi@gmail.com>
2025-04-28 09:23:33 +0300

    Showing the profile error in console

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 import { getUserProfile } from '@/actions/user-profile.action';
+import ErrorPage from '../error/page';
 import UserProfilePageDataContent from './UserProfilePageDataContent';
 
 export default async function Profile() {
   const profile = await getUserProfile();
 
   if (!profile.success) {
-    return <div>Error loading profile</div>;
+    console.error('Error fetching user profile:', profile.error);
+    return <ErrorPage />;
   }
 
   return <UserProfilePageDataContent userProfile={profile.data} />;
