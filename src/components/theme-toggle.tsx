ee921b2b051f81d1db4b070a193fd4cf0dde45fe
nazhin1382 <nazhin.abdolbaghi@gmail.com>
2025-04-28 07:56:50 +0300

    theme-toggle design has been modified

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,51 +1,61 @@
 'use client';
 
-import { Laptop, Moon, Sun } from 'lucide-react';
+import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from './ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
-import { SidebarMenuButton } from './ui/sidebar';
 
-export function ThemeToggle({ isSidebar = true }: { isSidebar?: boolean }) {
-  const { setTheme } = useTheme();
+export function ThemeToggle() {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  function handletheme(): void {
+    if (resolvedTheme === 'dark') {
+      setTheme('light');
+    } else if (resolvedTheme === 'light') {
+      setTheme('dark');
+    }
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        {isSidebar ? (
-          <SidebarMenuButton>
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span>Toggle theme</span>
-          </SidebarMenuButton>
-        ) : (
-          <Button>
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        )}
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          <Sun className="mr-2 h-4 w-4" />
-          <span>Light</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          <Moon className="mr-2 h-4 w-4" />
-          <span>Dark</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          <Laptop className="mr-2 h-4 w-4" />
-          <span>System</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    // <DropdownMenu>
+    //   <DropdownMenuTrigger asChild>
+    //     {isSidebar ? (
+    //       <SidebarMenuButton>
+    //         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    //         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    //         <span>Toggle theme</span>
+    //       </SidebarMenuButton>
+    //     ) : (
+    //       <Button>
+    //         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    //         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    //         <span className="sr-only">Toggle theme</span>
+    //       </Button>
+    //     )}
+    //   </DropdownMenuTrigger>
+    //   <DropdownMenuContent align="end">
+    //     <DropdownMenuItem onClick={() => setTheme('light')}>
+    //       <Sun className="mr-2 h-4 w-4" />
+    //       <span>Light</span>
+    //     </DropdownMenuItem>
+    //     <DropdownMenuItem onClick={() => setTheme('dark')}>
+    //       <Moon className="mr-2 h-4 w-4" />
+    //       <span>Dark</span>
+    //     </DropdownMenuItem>
+    //     <DropdownMenuItem onClick={() => setTheme('system')}>
+    //       <Laptop className="mr-2 h-4 w-4" />
+    //       <span>System</span>
+    //     </DropdownMenuItem>
+    //   </DropdownMenuContent>
+    // </DropdownMenu>
+    <Button
+      variant="ghost"
+      size="icon"
+      className="data-[state=open]:bg-accent data-[state=open]:text-accent-foreground float-left"
+      onClick={handletheme}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
   );
 }
